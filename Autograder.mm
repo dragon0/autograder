7ec803ee3947eef86b8a35cd55cc568a20434bcf
Raven <sravendragon0@hotmail.com>
2018-12-29 16:26:15 -0800

    added admin ui section

--- Autograder.mm
+++ Autograder.mm
@@ -1,34 +1,100 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1532488597656" ID="ID_335630043" MODIFIED="1532488607934" TEXT="Autograder">
-<node CREATED="1532488672732" ID="ID_1129978682" MODIFIED="1532493870284" POSITION="right" TEXT="Instructor Dashboard">
-<node CREATED="1532488714298" ID="ID_1769277855" MODIFIED="1532488722986" TEXT="Create Assignment">
-<node CREATED="1532488724580" ID="ID_864882972" MODIFIED="1532488730876" TEXT="Add Instructions"/>
-<node CREATED="1532493982416" ID="ID_1301374150" MODIFIED="1532493999071" TEXT="Enable/Disable Languages"/>
-<node CREATED="1532488731136" ID="ID_276955724" MODIFIED="1532488735878" TEXT="Add Ruberic"/>
-<node CREATED="1532488739327" ID="ID_1030145092" MODIFIED="1532488743279" TEXT="Add Public Tests"/>
-<node CREATED="1532488743902" ID="ID_677609685" MODIFIED="1532488747576" TEXT="Add Private Tests"/>
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Autograder" FOLDED="false" ID="ID_335630043" CREATED="1532488597656" MODIFIED="1532488607934"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Administrator Dashboard" POSITION="right" ID="ID_435509993" CREATED="1546122658475" MODIFIED="1546122667749">
+<node TEXT="Create Instructors" ID="ID_1644978005" CREATED="1546122669502" MODIFIED="1546122674228"/>
+<node TEXT="Create Students" ID="ID_207142982" CREATED="1546122674561" MODIFIED="1546122677531"/>
+<node TEXT="Assign Instructors/Students to Courses" ID="ID_1550507842" CREATED="1546122678294" MODIFIED="1546122690766"/>
 </node>
-<node CREATED="1532494055105" ID="ID_1294039497" MODIFIED="1532494059652" TEXT="Grade Assignment">
-<node CREATED="1532494202545" ID="ID_965331495" MODIFIED="1532494222585" TEXT="Show submission alongside ruberic">
-<node CREATED="1532494253203" ID="ID_905783906" MODIFIED="1532494271146" TEXT="Checkboxes on individual ruberic items to give credit"/>
+<node TEXT="Instructor Dashboard" POSITION="right" ID="ID_1129978682" CREATED="1532488672732" MODIFIED="1532493870284">
+<node TEXT="Create Assignment" ID="ID_1769277855" CREATED="1532488714298" MODIFIED="1532488722986">
+<node TEXT="Add Instructions" ID="ID_864882972" CREATED="1532488724580" MODIFIED="1532488730876"/>
+<node TEXT="Enable/Disable Languages" ID="ID_1301374150" CREATED="1532493982416" MODIFIED="1532493999071"/>
+<node TEXT="Add Ruberic" ID="ID_276955724" CREATED="1532488731136" MODIFIED="1532488735878"/>
+<node TEXT="Add Public Tests" ID="ID_1030145092" CREATED="1532488739327" MODIFIED="1532488743279"/>
+<node TEXT="Add Private Tests" ID="ID_677609685" CREATED="1532488743902" MODIFIED="1532488747576"/>
 </node>
-<node CREATED="1532494067358" ID="ID_885773212" MODIFIED="1532494088933" TEXT="Display Tests Passed/Failed"/>
-<node CREATED="1532494091077" ID="ID_280874042" MODIFIED="1532494136769" TEXT="Compare to other students&apos; code to flag plagiarism"/>
-<node CREATED="1532494146265" ID="ID_357996165" MODIFIED="1532494173707" TEXT="Add feedback comments to individual lines of code"/>
+<node TEXT="Grade Assignment" ID="ID_1294039497" CREATED="1532494055105" MODIFIED="1532494059652">
+<node TEXT="Show submission alongside ruberic" FOLDED="true" ID="ID_965331495" CREATED="1532494202545" MODIFIED="1532494222585">
+<node TEXT="Checkboxes on individual ruberic items to give credit" ID="ID_905783906" CREATED="1532494253203" MODIFIED="1532494271146"/>
 </node>
-<node CREATED="1532488756183" ID="ID_1248498550" MODIFIED="1532488893606" TEXT="List Assignments and Statistics"/>
-<node CREATED="1532488894140" ID="ID_831999484" MODIFIED="1532488904972" TEXT="Display Grade Table"/>
+<node TEXT="Display Tests Passed/Failed" ID="ID_885773212" CREATED="1532494067358" MODIFIED="1532494088933"/>
+<node TEXT="Compare to other students&apos; code to flag plagiarism" ID="ID_280874042" CREATED="1532494091077" MODIFIED="1532494136769"/>
+<node TEXT="Add feedback comments to individual lines of code" ID="ID_357996165" CREATED="1532494146265" MODIFIED="1532494173707"/>
 </node>
-<node CREATED="1532488700346" ID="ID_991303470" MODIFIED="1532493867367" POSITION="right" TEXT="Student Dashboard">
-<node CREATED="1532488914909" ID="ID_617350884" MODIFIED="1532488926332" TEXT="List Available / Completed Assignments"/>
-<node CREATED="1532488930393" ID="ID_985905842" MODIFIED="1532488933640" TEXT="Show Grades"/>
-<node CREATED="1532488933993" ID="ID_921563848" MODIFIED="1532488942303" TEXT="Show Instructor Feedback"/>
+<node TEXT="List Assignments and Statistics" ID="ID_1248498550" CREATED="1532488756183" MODIFIED="1532488893606"/>
+<node TEXT="Display Grade Table" ID="ID_831999484" CREATED="1532488894140" MODIFIED="1532488904972"/>
 </node>
-<node CREATED="1532488705090" ID="ID_1304804817" MODIFIED="1532492715539" POSITION="right" TEXT="Submission System">
-<node CREATED="1532488950783" ID="ID_475434842" MODIFIED="1532488956193" TEXT="Accept a Submission">
-<node CREATED="1532488957676" ID="ID_504644583" MODIFIED="1532493938468" TEXT="Code">
-<richcontent TYPE="NOTE"><html>
+<node TEXT="Student Dashboard" FOLDED="true" POSITION="right" ID="ID_991303470" CREATED="1532488700346" MODIFIED="1532493867367">
+<node TEXT="List Available / Completed Assignments" ID="ID_617350884" CREATED="1532488914909" MODIFIED="1532488926332"/>
+<node TEXT="Show Grades" ID="ID_985905842" CREATED="1532488930393" MODIFIED="1532488933640"/>
+<node TEXT="Show Instructor Feedback" ID="ID_921563848" CREATED="1532488933993" MODIFIED="1532488942303"/>
+</node>
+<node TEXT="Submission System" FOLDED="true" POSITION="right" ID="ID_1304804817" CREATED="1532488705090" MODIFIED="1532492715539">
+<node TEXT="Accept a Submission" FOLDED="true" ID="ID_475434842" CREATED="1532488950783" MODIFIED="1532488956193">
+<node TEXT="Code" ID="ID_504644583" CREATED="1532488957676" MODIFIED="1532493938468"><richcontent TYPE="NOTE">
+
+<html>
   <head>
     
   </head>
@@ -40,8 +106,9 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1532488963953" ID="ID_467280088" MODIFIED="1532493973610" TEXT="Language">
-<richcontent TYPE="NOTE"><html>
+<node TEXT="Language" ID="ID_467280088" CREATED="1532488963953" MODIFIED="1532493973610"><richcontent TYPE="NOTE">
+
+<html>
   <head>
     
   </head>
@@ -53,16 +120,17 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1532488967559" ID="ID_1324444118" MODIFIED="1532488973470" TEXT="Compile/Run Code"/>
-<node CREATED="1532488974922" ID="ID_1655878296" MODIFIED="1532493815031" TEXT="Execute Public and Secret Tests">
-<node CREATED="1532493476528" ID="ID_65061495" MODIFIED="1532493483205" TEXT="Types of Tests">
-<node CREATED="1532493487355" ID="ID_955422016" MODIFIED="1532493491321" TEXT="Unit Tests">
-<node CREATED="1532493492476" ID="ID_767180161" MODIFIED="1532493495145" TEXT="BoostTest"/>
-<node CREATED="1532493495458" ID="ID_392602879" MODIFIED="1532493497574" TEXT="JUnit"/>
-<node CREATED="1532493498274" ID="ID_457158596" MODIFIED="1532493503284" TEXT="unittest.py"/>
+<node TEXT="Compile/Run Code" ID="ID_1324444118" CREATED="1532488967559" MODIFIED="1532488973470"/>
+<node TEXT="Execute Public and Secret Tests" FOLDED="true" ID="ID_1655878296" CREATED="1532488974922" MODIFIED="1532493815031">
+<node TEXT="Types of Tests" FOLDED="true" ID="ID_65061495" CREATED="1532493476528" MODIFIED="1532493483205">
+<node TEXT="Unit Tests" FOLDED="true" ID="ID_955422016" CREATED="1532493487355" MODIFIED="1532493491321">
+<node TEXT="BoostTest" ID="ID_767180161" CREATED="1532493492476" MODIFIED="1532493495145"/>
+<node TEXT="JUnit" ID="ID_392602879" CREATED="1532493495458" MODIFIED="1532493497574"/>
+<node TEXT="unittest.py" ID="ID_457158596" CREATED="1532493498274" MODIFIED="1532493503284"/>
 </node>
-<node CREATED="1532493512077" ID="ID_816210704" MODIFIED="1532493565722" TEXT="pexpect tests">
-<richcontent TYPE="NOTE"><html>
+<node TEXT="pexpect tests" ID="ID_816210704" CREATED="1532493512077" MODIFIED="1532493565722"><richcontent TYPE="NOTE">
+
+<html>
   <head>
     
   </head>
@@ -74,8 +142,9 @@
 </html>
 </richcontent>
 </node>
-<node CREATED="1532493516714" ID="ID_529075706" MODIFIED="1532493812556" TEXT="stdio tests">
-<richcontent TYPE="NOTE"><html>
+<node TEXT="stdio tests" ID="ID_529075706" CREATED="1532493516714" MODIFIED="1532493812556"><richcontent TYPE="NOTE">
+
+<html>
   <head>
     
   </head>
@@ -89,11 +158,11 @@
 </node>
 </node>
 </node>
-<node CREATED="1532489012744" ID="ID_1941540090" MODIFIED="1532489019230" TEXT="Report on Grade">
-<node CREATED="1532493825192" ID="ID_596890007" MODIFIED="1532493838154" TEXT="Grading Options">
-<node CREATED="1532493839005" ID="ID_230782160" MODIFIED="1532493845289" TEXT="No credit if tests fail"/>
-<node CREATED="1532493847802" ID="ID_1846012430" MODIFIED="1532493855828" TEXT="Partial credit based on number of passed tests"/>
-<node CREATED="1532493856514" ID="ID_1290489114" MODIFIED="1532493862543" TEXT="Extra credit for certain tests"/>
+<node TEXT="Report on Grade" FOLDED="true" ID="ID_1941540090" CREATED="1532489012744" MODIFIED="1532489019230">
+<node TEXT="Grading Options" FOLDED="true" ID="ID_596890007" CREATED="1532493825192" MODIFIED="1532493838154">
+<node TEXT="No credit if tests fail" ID="ID_230782160" CREATED="1532493839005" MODIFIED="1532493845289"/>
+<node TEXT="Partial credit based on number of passed tests" ID="ID_1846012430" CREATED="1532493847802" MODIFIED="1532493855828"/>
+<node TEXT="Extra credit for certain tests" ID="ID_1290489114" CREATED="1532493856514" MODIFIED="1532493862543"/>
 </node>
 </node>
 </node>
